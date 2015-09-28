6fe50cd0740a0aed592f55859ccad4f1c1b13926
Troy Starwalt <troy.starwalt@gmail.com>
2015-09-28 13:33:41 -0400

    Initial Commit

---
+++ forloops.js
@@ -0,0 +1,13 @@
+var x = ["crazy", 4, "dumb", "smart"]
+var y = ["short", 8, "tall", "medium"]
+var atts = [x, y]
+
+function arrayloop(){
+	for (var i = 0; i >= atts.length; i+=1) {
+		x+y;
+		console.log(i);
+	};
+}
+
+arrayloop()
+
